a10ca6150b277d04b91d726efccd247a854e41c3
RadianArgha <coinbuysellbazar@gmail.com>
2025-10-12 21:21:43 +0600

    setting up basic tempproject

---
+++ basicProjects/tempConverter/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Enter a temperature calculator");
+    println!("1. Celsius to Fahrenheit");
+    println!("2. Fahrenheit to Celsius");
+    printlm!("Enter a  choice between 1 and 2: ");
+    
+
+}
